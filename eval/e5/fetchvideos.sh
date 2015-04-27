f38cde50ed00cd3da44e02c172444c4c7a31323a
Andrew Head <head.andrewm@gmail.com>
2015-04-26 21:08:05 -0700

    Evaluations: #5, got frames of menu appearance and bounding rects.

---
+++ eval/e5/fetchvideos.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+RATE_LIMIT='400k'
+HEIGHT='480'
+YOUTUBE="https://www.youtube.com/watch?v="
+OUTDIR="videos"
+VID_IDS=(
+'vXcC533_IcQ'
+'o1vRJ07KDZ0'
+'-MBS2YvUY2M'
+'p9KRJoX13vo'
+'ok8mZW_BDG4'
+)
+LOGFILE='.download.log'
+
+for vid in "${VID_IDS[@]}"
+do
+  echo "=== Downloading video $vid ==="
+  youtube-dl \
+    --rate-limit $RATE_LIMIT \
+    -f "best[height=$HEIGHT]+bestaudio" \
+    -o "videos/%(autonumber)s-%(id)s-%(title)s.%(ext)s" \
+    --autonumber-size 1 \
+    --restrict-filenames \
+    --no-overwrites \
+    "$YOUTUBE$vid" \
+    2>&1 | tee -a $LOGFILE
+done
